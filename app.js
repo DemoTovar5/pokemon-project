1c63d08c5538e984e99e3d5db22cb1c54c250cb5
DemoTovar5 <demetrio.tovar5@gmail.com>
2021-08-10 10:17:38 -0500

    changes

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-function getPokemonData(pokemon){
-    let url = pokemon.url
-    fetch(url)
-        .then(response => response.json())
-        .then(function(pokeData){
-            console.log(pokeData)
-        })
+$("catch-btn").click(function (){
+    getPokemon(Math.floor(Math.random()* 100)+1)
+});
+
+function addPokemon (data){
+    $("poke-container").append(`
+    <div class="pokemon">
+    <h3>${data.name}<br></h3>
+    Type: ${data.types[0].type.name}<br>
+    Top moves: <br> ${data.moves[0].move.name} <br>
+    ${data.moves[1].move.name} <br>
+    ${data.moves[2].move.name} <br>
+    </div>      
+    `)
 }
--- pokemon-utils.js
+++ pokemon-utils.js
@@ -1,10 +1,15 @@
 function getPokemon(){
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-        .then(response => response.json())
-        .then(function(allpokemon){
-            allpokemon.results.forEach(function(pokemon){
-                getPokemonData(pokemon);
-            })
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`,{
+        method: "GET"
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            addPokemon(data);
         })
+        .catch(err => {
+            console.log("YOU HAVE AN ERROR: ", err);
+        })
+
 }
 
